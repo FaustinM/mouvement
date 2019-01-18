d15297385b5ee74c0eaa6e0da7bfdb0acb8f7beb
Unknown <faustino40@outlook.fr>
2019-01-18 18:16:19 +0100

    C'est partit
    - Envoie de tout les fichiers du site web !

---
+++ assets/dicColor.js
@@ -0,0 +1,37 @@
+const dicColor = [{
+        nom: "rouge",
+        hex: "#f44336"
+    },
+    {
+        nom: "rose",
+        hex: "#E91E63"
+    },
+    {
+        nom: "violet",
+        hex: "#9C27B0"
+    },
+    {
+        nom: "bleu",
+        hex: "#2196F3"
+    },
+    {
+        nom: "vert",
+        hex: "#4CAF50"
+    },
+    {
+        nom: "jaune",
+        hex: "#FFEB3B"
+    },
+    {
+        nom: "orange",
+        hex: "#FF9800"
+    },
+    {
+        nom: "gris",
+        hex: "#9E9E9E"
+    },
+    {
+        nom: "noir",
+        hex: "#212121"
+    },
+]
